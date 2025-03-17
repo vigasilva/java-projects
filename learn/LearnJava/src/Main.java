0f7469a59068f33c8ae461c8544ea64609900b69
vigasilva <138346525+vigasilva@users.noreply.github.com>
2025-03-17 03:06:32 -0300

    Update Main.java

--- learn/LearnJava/src/Main.java
+++ learn/LearnJava/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in); //opens the scanner for input listening
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print("Enter your name:");
+        String name = scanner.nextLine(); // asking for user name
+
+        System.out.print("Enter your age:");
+        int age = scanner.nextInt(); // asking for user age
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old."); // greets the user based on gathered information
     }
 }
